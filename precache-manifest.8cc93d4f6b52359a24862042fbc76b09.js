5c6f55636a6cfcd5116a2f414cb1729b073da67f
Michel Reyes <mreyesh85@gmail.com>
2020-03-23 01:39:49 -0400

    Updates

---
+++ precache-manifest.8cc93d4f6b52359a24862042fbc76b09.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5e872402f073ef79b4f15ed6cca5f2c",
+    "url": "/m-reyes-h/weather/index.html"
+  },
+  {
+    "revision": "ac8c7a3e5d16a58174ca",
+    "url": "/m-reyes-h/weather/static/css/main.46665dd6.chunk.css"
+  },
+  {
+    "revision": "cb09f7bab5cb9b9b549c",
+    "url": "/m-reyes-h/weather/static/js/2.6f3fe080.chunk.js"
+  },
+  {
+    "revision": "413222bfbc1dccea2ee6791f9b56cfbf",
+    "url": "/m-reyes-h/weather/static/js/2.6f3fe080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac8c7a3e5d16a58174ca",
+    "url": "/m-reyes-h/weather/static/js/main.546384a7.chunk.js"
+  },
+  {
+    "revision": "4537bed1dcf3673d714d",
+    "url": "/m-reyes-h/weather/static/js/runtime-main.9c38bd17.js"
+  },
+  {
+    "revision": "fc726b198ebf4d22af3d89bbed812e32",
+    "url": "/m-reyes-h/weather/static/media/broken.fc726b19.svg"
+  },
+  {
+    "revision": "33713b750e9639aad69c76c72ca4e8d6",
+    "url": "/m-reyes-h/weather/static/media/cloud.33713b75.svg"
+  },
+  {
+    "revision": "28e64f454a29782d7d600b408b4b1037",
+    "url": "/m-reyes-h/weather/static/media/foga.28e64f45.svg"
+  },
+  {
+    "revision": "0bd1fcfdafed7dc4880c99344e9c3fee",
+    "url": "/m-reyes-h/weather/static/media/fogb.0bd1fcfd.svg"
+  },
+  {
+    "revision": "efdbdb2a7d214528670ffee130811bcf",
+    "url": "/m-reyes-h/weather/static/media/fogc.efdbdb2a.svg"
+  },
+  {
+    "revision": "7aa68b094b466cb1d15fc2059ad7caf0",
+    "url": "/m-reyes-h/weather/static/media/inner.7aa68b09.svg"
+  },
+  {
+    "revision": "827e490680d26b64b5a8e5f70fecd301",
+    "url": "/m-reyes-h/weather/static/media/inner.827e4906.svg"
+  },
+  {
+    "revision": "e56090f2438c639f14e2967ee0d90ae4",
+    "url": "/m-reyes-h/weather/static/media/inner.e56090f2.svg"
+  },
+  {
+    "revision": "f1bb11cb61d0e57ab43d5554cf7a7d36",
+    "url": "/m-reyes-h/weather/static/media/inner.f1bb11cb.svg"
+  },
+  {
+    "revision": "3ced1b10c4084df51c3b0a76b02228f2",
+    "url": "/m-reyes-h/weather/static/media/moon.3ced1b10.svg"
+  },
+  {
+    "revision": "22a3ec58b9ee13009e00aa263d256341",
+    "url": "/m-reyes-h/weather/static/media/out.22a3ec58.svg"
+  },
+  {
+    "revision": "4fdc50b35513041a699a927d6e538db8",
+    "url": "/m-reyes-h/weather/static/media/out.4fdc50b3.svg"
+  },
+  {
+    "revision": "967970b23444554f14300aa5f9698a15",
+    "url": "/m-reyes-h/weather/static/media/out.967970b2.svg"
+  },
+  {
+    "revision": "ad9ebae16ddc1185645636699a5c2631",
+    "url": "/m-reyes-h/weather/static/media/out.ad9ebae1.svg"
+  },
+  {
+    "revision": "00996941ffea9a05ff589830d904518a",
+    "url": "/m-reyes-h/weather/static/media/outer.00996941.svg"
+  },
+  {
+    "revision": "e7b375397a583733e50d4740e4493ab3",
+    "url": "/m-reyes-h/weather/static/media/rain.e7b37539.svg"
+  },
+  {
+    "revision": "8918688812ffc0a43e1625b096260f78",
+    "url": "/m-reyes-h/weather/static/media/star.89186888.svg"
+  },
+  {
+    "revision": "4f852b4d7f8a9ed3f021ab82fa0a1d2a",
+    "url": "/m-reyes-h/weather/static/media/sunInner.4f852b4d.svg"
+  },
+  {
+    "revision": "46c75ba3a233cfc26e8de4982e842ce5",
+    "url": "/m-reyes-h/weather/static/media/sunOuter.46c75ba3.svg"
+  },
+  {
+    "revision": "35c7a5a3a365ac9a20c2ee5e1c1ec419",
+    "url": "/m-reyes-h/weather/static/media/thunder.35c7a5a3.svg"
+  },
+  {
+    "revision": "3bf744f4ed471dfbe4e61ade241aef99",
+    "url": "/m-reyes-h/weather/static/media/umbrella.3bf744f4.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/m-reyes-h/weather/precache-manifest.8cc93d4f6b52359a24862042fbc76b09.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/m-reyes-h/weather/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
